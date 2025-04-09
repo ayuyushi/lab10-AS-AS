8526c7190dfea007a00ae9ffc2bb76a182cf3b1e
ayuyushi <srivastavaayushi@ufl.edu>
2025-04-09 15:26:29 -0400

    need to add some more functions

--- calculator.py
+++ calculator.py
@@ -7,6 +7,15 @@ One function per operation, in order.
 import math
 
 # First example
+def square_root(a): 
+    math.sqrt(a)
+    if a < 0:
+        raise ValueError 
+        
+def hypotenuse(a, b): 
+    math.hypot(a, b) 
+
+
 def add(a, b): 
     return a + b
 
