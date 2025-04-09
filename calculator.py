a70c460c536e02f10575835ba73f152abdcdae54
ayuyushi <srivastavaayushi@ufl.edu>
2025-04-09 15:29:06 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -22,14 +22,6 @@ def add(a, b):
 def subtract(a, b): 
     return a - b
 
-def multiply(a, b): 
-    return a * b
-
-def divide(a, b): 
-    try:
-        return b / a
-        if a == 0:
-            raise ZeroDivisionError 
 
 def logarithm(a, b): loga(b)
     try:
@@ -37,14 +29,6 @@ def logarithm(a, b): loga(b)
         if b <= 0 or a <= 0:
             raise ValueError
 
-def exponent(a, b): 
-    return a ** b
-
-def add(a, b):
-    return a + b
-
-def sub(a, b):
-    return a - b
 
 def mul(a, b):
     return a * b
@@ -53,13 +37,6 @@ def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b / a
-
-def log(a, b):
-    if a <= 0 or a == 1:
-        raise ValueError
-    if b <=0:
-        raise ValueError
-    return math.log(b, a)
 
 def exp(a, b):
     return a ** b
